4c59bd4540e18a985b535f5d9e03d1bea796a724
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-13 15:27:30 +0200

    first try

--- main.py
+++ main.py
@@ -1,12 +1,60 @@
-def calculate():
+import asyncio
+import httpx
+
+
+def api_response_callback(response_data):
     """
-    calculates the total
-    :return: None
+    Callback-Funktion, die aufgerufen wird, nachdem die API-Antwort empfangen wurde.
+
+    Args:
+    - response_data: Die Daten, die von der API empfangen wurden.
+
+    Returns:
+    - None, da die Daten direkt in der Konsole ausgegeben werden.
     """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+    #TODO: Hier die Daten verarbeiten
+
+async def fetch_data_from_api(callback):
+    """
+    Diese Funktion ruft asynchron alle 3 Sekunden eine API ('https://hub.dummyapis.com/delay?seconds=3') auf, die eine
+    Verzögerung von 3 Sekunden simuliert. Nachdem die Daten von der API abgerufen wurden, wird der bereitgestellte
+    Callback mit den Daten aufgerufen.
+
+    Args:
+    - callback: Die Callback-Funktion, die aufgerufen wird, nachdem die API-Daten empfangen wurden.
+
+    Returns:
+    - None, da die Daten an die Callback-Funktion weitergegeben werden.
+    """
+    # TODO: Hier in einer Endlosschleife die API aufrufen und die Daten an die Callback-Funktion übergeben
+
+
+async def async_timer():
+    """
+    Diese Funktion fungiert als asynchroner Timer, der jede Sekunde hochzählt und den aktuellen Wert ausgibt.
+    Sie verwendet `asyncio.sleep` für die Verzögerung und führt eine endlose Schleife aus, die den Zähler jede Sekunde erhöht.
+
+    Returns:
+    - None, da der Zählerstand direkt in der Konsole ausgegeben wird.
+    """
+    #TODO: Hier den Timer implementieren
+
+
+async def main():
+    """
+    Hauptfunktion, die beide asynchrone Funktionen, `fetch_data_from_api` und `async_timer`, parallel ausführt.
+    Sie verwendet `asyncio.create_task` um die beiden Funktionen als separate, gleichzeitig laufende Tasks zu starten.
+
+    Returns:
+    - None, da alle Ausgaben direkt in den jeweiligen Funktionen erfolgen.
+    """
+
+    api_task = asyncio.create_task(fetch_data_from_api(api_response_callback))
+    timer_task = asyncio.create_task(async_timer())
+
+    await api_task
+    await timer_task
 
 
 if __name__ == '__main__':
-    calculate()
+    asyncio.run(main())
--- main_test.py
+++ main_test.py
@@ -1,6 +1,37 @@
-import main
+import pytest
+from unittest.mock import Mock, patch, AsyncMock
+from sol import api_response_callback, async_timer, fetch_data_from_api
 
-def test(capsys):
-    main.calculate()
+
+# 1. Test für api_response_callback
+def test_api_response_callback(capsys):
+    api_response_callback({"message": "test"})
     captured = capsys.readouterr()
-    assert captured.out == "Total: 78.75\n"
+    assert "API Response: {'message': 'test'}" in captured.out
+
+
+# 2. Test für async_timer
+@pytest.mark.asyncio
+async def test_async_timer(capsys):
+    with patch('asyncio.sleep', side_effect=[None, None, StopAsyncIteration]):
+        try:
+            await async_timer()
+        except StopAsyncIteration:
+            pass
+    captured = capsys.readouterr()
+    assert "0\n1\n" in captured.out
+
+
+# 3. Test für fetch_data_from_api
+@pytest.mark.asyncio
+async def test_fetch_data_from_api():
+    mock_callback = Mock()
+    mock_response = Mock()
+    mock_response.text = "test response"  # Setzen Sie den Response-Text
+    with patch('httpx.AsyncClient', return_value=Mock(get=AsyncMock(return_value=mock_response))), \
+         patch('asyncio.sleep', side_effect=[None, StopAsyncIteration]):
+        try:
+            await fetch_data_from_api(mock_callback)
+        except StopAsyncIteration:
+            pass
+    mock_callback.assert_called_with(mock_response)
